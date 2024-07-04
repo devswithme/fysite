df0c16d9ea518ea8086b7bbf01e27993af84a17e
devswithme <developswithme@gmail.com>
2024-07-04 13:28:03 +0800

    add work link preview

--- src/App.tsx
+++ src/App.tsx
@@ -60,9 +60,57 @@ function App() {
 					<section
 						id='ourwork'
 						className='grid grid-cols-1 md:grid-cols-3 gap-4 max-w-screen-lg mx-auto py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20'>
-						<img src='work/1.png' />
-						<img src='work/2.png' />
-						<img src='work/3.png' />
+						<div className='relative group overflow-hidden'>
+							<img
+								src='work/1.png'
+								className='group-hover:blur-sm transition-all'
+							/>
+							<a
+								href='https://chakra-preview.vercel.app'
+								target='_blank'
+								className={buttonVariants({
+									size: 'sm',
+
+									className:
+										'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] opacity-0 group-hover:opacity-100 transition-all',
+								})}>
+								View Site <ArrowUpRight className='w-4 h-4 ml-2' />
+							</a>
+						</div>
+						<div className='relative group overflow-hidden'>
+							<img
+								src='work/2.png'
+								className='group-hover:blur-sm transition-all'
+							/>
+							<a
+								href='https://rcc-rho.vercel.app'
+								target='_blank'
+								className={buttonVariants({
+									size: 'sm',
+
+									className:
+										'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] opacity-0 group-hover:opacity-100 transition-all',
+								})}>
+								View Site <ArrowUpRight className='w-4 h-4 ml-2' />
+							</a>
+						</div>
+						<div className='relative group overflow-hidden'>
+							<img
+								src='work/3.png'
+								className='group-hover:blur-sm transition-all'
+							/>
+							<a
+								href='https://thelittlecloud.vercel.app'
+								target='_blank'
+								className={buttonVariants({
+									size: 'sm',
+
+									className:
+										'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] opacity-0 group-hover:opacity-100 transition-all',
+								})}>
+								View Site <ArrowUpRight className='w-4 h-4 ml-2' />
+							</a>
+						</div>
 						<img src='work/4.png' />
 					</section>
 					<section
